023724a8e222dcc362c27ffa3976f8205e81c94e
Anulo2 <leonardocanello@gmail.com>
2022-02-07 16:34:00 +0100

    Fix sistemaambientelucca link

--- point1.py
+++ point1.py
@@ -2,5 +2,5 @@
 import requests
 url = 'https://indicepa.gov.it/ipa-dati/dataset/502ff370-1b2c-4310-94c7-f39ceb7500e3/resource/3ed63523-ff9c-41f6-a6fe-980f3d9e501f/download/amministrazioni.txt'
 r = requests.get(url, allow_redirects=True)
-open('amministrazioni.txt', 'wb').write(r.content)
-
+result = r.content.replace(b'HTTPS:SISTEMAAMBIENTELUCCA.IT', b'HTTPS://SISTEMAAMBIENTELUCCA.IT')
+open('amministrazioni.txt', 'wb').write(result)
