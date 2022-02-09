2c3b5a78e5a5bcef5b3858c998c1f630d62206d4
Giacomo Tesio <giacomo@tesio.it>
2022-02-09 22:08:04 +0000

    add missing license header

--- cli/commons.py
+++ cli/commons.py
@@ -1,3 +1,10 @@
+# This file is part of MonitoraPA
+#
+# Copyright (C) 2022 Giacomo Tesio <giacomo@tesio.it>
+#
+# MonitoraPA is a hack. You can use it according to the terms and
+# conditions of the Hacking License (see LICENSE.txt)
+
 import datetime
 import os
 import os.path
