ea15a8fa61fc9d34ea4f8554154f4453bb5dcc32
Giacomo Tesio <giacomo@tesio.it>
2022-03-08 08:03:09 +0100

    cli/crontab.sh: fix enti.tsv path

--- cli/crontab.sh
+++ cli/crontab.sh
@@ -6,13 +6,13 @@ if [ "x" == "x$LOG" ]; then
     LOG="out/monitorapa.$PID.$DATE.log"
 fi
 
-CURRENT_DIR=$(./cli/point1.py |tee -a $LOG)
+CURRENT_ENTI=$(./cli/point1.py |tee -a $LOG)
 
-if [ ! -f "$CURRENT_DIR/enti.tsv" ]; then
-    echo "Error in point1: no enti.tsv in $CURRENT_DIR" | tee -a $LOG
+if [ ! -f "$CURRENT_ENTI" ]; then
+    echo "Error in point1: no $CURRENT_DIR" | tee -a $LOG
     exit 1
 fi
 
-python3 -u ./cli/point2parallel.py check/google_analytics.js $CURRENT_DIR/enti.tsv 13000 >> $LOG 2>&1
+python3 -u ./cli/point2parallel.py check/google_analytics.js $CURRENT_ENTI 13000 >> $LOG 2>&1
 
-# ./cli/point4.py check/google_analytics.js $CURRENT_DIR/enti.tsv 30 >> $LOG 2>&1
+# ./cli/point4.py check/google_analytics.js $CURRENT_ENTI 30 >> $LOG 2>&1
