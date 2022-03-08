03d567f2a0d48bb75f2335d33412eda364f04883
Giacomo Tesio <giacomo@tesio.it>
2022-03-08 07:40:59 +0100

    add cli/crontab.sh

---
+++ cli/crontab.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+LOG="$1"
+if [ "x" == "x$LOG" ]; then
+    DATE=$(date "+%Y-%m-%d")
+    LOG="out/monitorapa.$PID.$DATE.log"
+fi
+
+CURRENT_DIR=$(./cli/point1.py |tee -a $LOG|awk '{print $4}')
+
+if [ ! -f "$CURRENT_DIR/enti.tsv" ]; then
+    echo "Error in point1: no enti.tsv in $CURRENT_DIR" | tee -a $LOG
+    exit 1
+fi
+
+python3 -u ./cli/point2parallel.py check/google_analytics.js $CURRENT_DIR/enti.tsv 13000 >> $LOG 2>&1
+
+# ./cli/point4.py check/google_analytics.js $CURRENT_DIR/enti.tsv 30 >> $LOG 2>&1
