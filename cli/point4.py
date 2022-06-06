347c24f204d4fc10095bc2e8399d60470f30240e
Giacomo Tesio <giacomo@tesio.it>
2022-06-06 06:11:38 +0000

    ./cli/point2.py: add some missing copyright statements

--- cli/point4.py
+++ cli/point4.py
@@ -3,6 +3,9 @@
 # This file is part of MonitoraPA
 #
 # Copyright (C) 2022 Leonardo Canello <leonardocanello@protonmail.com>
+# Copyright (C) 2022 Stefano Gazzella <stefano@gdpready.it>
+# Copyright (C) 2022 Mario Sabatino <mario@sabatino.pro>
+# Copyright (C) 2022 Giacomo Tesio <giacomo@tesio.it>
 #
 # MonitoraPA is a hack. You can use it according to the terms and
 # conditions of the Hacking License (see LICENSE.txt)
